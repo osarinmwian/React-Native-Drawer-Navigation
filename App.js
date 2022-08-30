6cbbc462f0a2506c49a6bb6d13537f28b93eefb8
osarinmwian <noel.osarinmwian@gmail.com>
2022-08-30 20:53:38 +0100

    navigation_Drawer

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native'
+import Home from './screen/Home';
+import Settings from './screen/Settings';
+import FontaAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
-export default function App() {
+
+const Drawer= createDrawerNavigator();
+ function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   
+    <NavigationContainer>
+      <Drawer.Navigator
+      initialRouteName='Home'
+      drawerPosition='left'
+      drawerType='font'
+      edgeWidth={100}
+      overLayColor="#00000090"
+      drewerStyle={{
+        backgroundColor:"#e6e6e6",
+        width: 250
+      }}
+      screenOptions={{
+      headerShown: true,
+      swipeEnabled: true,
+      gestureEnabled: true,
+      headerTitleAlign:"center",
+      headerStyle:{
+        backgroundColor:"#0080ff"
+      },
+      headerTintColor:"#e6e6e6",
+      // headerTitle: ()=>null,
+      headerTitleStyle:{
+        fontSize:25,
+        fontWeight:'bold'
+
+      }
+      }}
+      >
+        <Drawer.Screen
+        name='home'
+        component={Home}
+        options={{
+
+          title:()=> null,
+          title:'Home Page',
+          drawerIcon:({focused})=>(
+             <FontaAwesome5
+             name='home'
+             size={focused ? 25: 20}
+             color={focused ? '#0080ff':'#999999'}/>
+          )
+        }}/>
+          <Drawer.Screen
+        name='settings'
+        component={Settings}
+        options={{
+          // header:()=> null,
+          title:'Settings',
+          drawerIcon:({focused})=>(
+             <FontaAwesome5
+             name='cog'
+             size={focused ? 25: 20}
+             color={focused ? '#0080ff':'#999999'}/>
+          )
+        }}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
